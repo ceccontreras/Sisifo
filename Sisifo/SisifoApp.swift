b078038280ebdc04a1ee7c6f8600695a0c530dd6
Carlos Campos <ceccontreras5@gmail.com>
2026-01-06 21:08:19 -0500

    Initial Commit

---
+++ Sisifo/SisifoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SisifoApp.swift
+//  Sisifo
+//
+//  Created by Carlos Campos on 1/6/26.
+//
+
+import SwiftUI
+
+@main
+struct SisifoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
